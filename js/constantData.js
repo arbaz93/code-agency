f1a93792d8f0c3fce5be853d11f73ad305b1688c
arbaz93 <arbaz.k480@yahoo.com>
2024-11-19 16:54:03 +0500

    " Added content in contact page "

--- js/constantData.js
+++ js/constantData.js
@@ -1,5 +1,5 @@
 const data = {
-    contactEmail: 'xxxx@codebeast.com',
+    contactEmail: ['xxxx@codebeast.com', 'yyyy@codebeast.com'],
     subsriptionEmail: 'xxxx@codebeast.com',
     phone: ['+92 900 0901', '90788888'],
     address: ['somewhere sometime', 'somewhere sometime 2'],
@@ -13,7 +13,9 @@ const data = {
 const submissionFormElements = document.querySelectorAll('#wf-form-contact');
 const address_1 = document.querySelectorAll('.address_1');
 const email_1 = document.querySelectorAll('.email_1');
+const email_2 = document.querySelectorAll('.email_2');
 const phone_1 = document.querySelectorAll('.phone_1');
+const phone_2 = document.querySelectorAll('.phone_2');
 const facebook = document.querySelectorAll('.facebook_link');
 const linkedin = document.querySelectorAll('.linkedin_link');
 const behance = document.querySelectorAll('.behance_link');
@@ -31,15 +33,26 @@ behance.forEach(link => {
 twitter.forEach(link => {
     link.href = data.socialLinks.twitter
 })
-address_1.forEach(address => {
+
+
+address_1?.forEach(address => {
     address.innerHTML = data.address[0];
 })
-email_1.forEach(email => {
-    email.innerHTML = data.contactEmail;;
+
+email_1?.forEach(email => {
+    email.innerHTML = data.contactEmail[0];;
 })
-phone_1.forEach(phone => {
+email_2?.forEach(email => {
+    email.innerHTML = data.contactEmail[1];;
+})
+
+phone_1?.forEach(phone => {
     phone.innerHTML = data.phone[0];
 })
+phone_2?.forEach(phone => {
+    phone.innerHTML = data.phone[1];
+})
+
 
 submissionFormElements.forEach(form => {
     console.log(form)

561632fb842feb45d5c2d8c593b0cf078e89b4cc
arbaz93 <arbaz.k480@yahoo.com>
2025-02-16 16:38:36 +0500

    added an arrow to navigation services link

--- js/navigation.js
+++ js/navigation.js
@@ -1,14 +1,15 @@
 const serviceLink = document.querySelector('.item-with-submenu');
 const subServices = document.querySelector('.sub-services'); 
+const dataShow = document.querySelectorAll('[data-show]'); 
 
 // Handling navigation sub menu
 const toggleServicesDisplay = (e) => {
     e.preventDefault();
     const toggleState = subServices.getAttribute('data-show');
     if(toggleState == 'false') {
-        subServices.setAttribute('data-show', true)
+        dataShow.forEach(attr => attr.setAttribute('data-show', true));
     } else {
-        subServices.setAttribute('data-show', false)
+        dataShow.forEach(attr => attr.setAttribute('data-show', false));
     }
 }
 serviceLink.addEventListener('click',  toggleServicesDisplay);
